1c3f444978d2f2972e7b34b8154ac406ee0c35ea
Ariel Weiss <ariel.weiss@gmail.com>
2021-06-16 23:02:36 +0300

    improvements

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { AlertController, Platform } from '@ionic/angular';
 import { AuthenticationService } from '../services/authentication.service';
 import { FirestoreService } from '../services/firestore.service';
 
@@ -29,7 +29,8 @@ export class Tab5Page {
   constructor(
     private authenticationService: AuthenticationService,
     private firestoreService: FirestoreService,
-    private platform: Platform
+    private platform: Platform,
+    private alertController: AlertController
   ) {}
 
   async ngOnInit() {
@@ -66,6 +67,12 @@ export class Tab5Page {
           this.groupUsers = users.filter(
             (user: any) => user.uid !== this.currUser.uid
           );
+          if (this.groupUsers.length === 0) {
+            this.groupUsers = [
+              { name: '-- זה הזמן להזמין חברים --' },
+              { name: `קוד קבוצה: ${groupCode}` },
+            ];
+          }
         });
     }
   }
@@ -73,6 +80,43 @@ export class Tab5Page {
   async updateGroupCode() {
     this.currUser.groupCode = this.groupCode;
     await this.firestoreService.createOrUpdateUser(this.currUser);
+  }
+
+  async editGroup() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'החלפת קבוצה',
+      inputs: [
+        {
+          name: 'newGroupCode',
+          type: 'text',
+          id: 'newGroupCode',
+          value: this.groupCode,
+          // placeholder: 'Placeholder 2',
+        },
+      ],
+      buttons: [
+        {
+          text: 'ביטול',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          },
+        },
+        {
+          text: 'שמור',
+          handler: async (alertData) => {
+            this.currUser.groupCode = alertData.newGroupCode;
+            await this.firestoreService.createOrUpdateUser(this.currUser);
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+    // this.currUser.groupCode = this.groupCode;
+    // await this.firestoreService.createOrUpdateUser(this.currUser);
   }
 
   logout() {
